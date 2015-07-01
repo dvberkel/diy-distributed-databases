e4fe4b3481c37389d69336992e3ddeffcdb157a7
dvberkel <daan.v.berkel.1980@gmail.com>
2015-07-01 14:54:58 +0100

    Create a ReturnMostRecent resolver test

---
+++ src/test/java/com/github/seeemilyplay/diydistdb/ReturnMostRecentTest.java
@@ -0,0 +1,25 @@
+package com.github.seeemilyplay.diydistdb;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ReturnMostRecentTest {
+    @Test
+    public void shouldResolveTheMostRecentThing() {
+        Resolver resolver = new ReturnMostRecent();
+        Thing expectedThing = new Thing(0, "baz", Long.MAX_VALUE);
+
+        Thing resolvedThing = resolver.resolve(Arrays.asList(
+                new Thing(0, "foo", 0L),
+                new Thing(1, "bar", 1L),
+                expectedThing
+        ));
+
+        assertThat(resolvedThing, is(expectedThing));
+
+    }
+}
