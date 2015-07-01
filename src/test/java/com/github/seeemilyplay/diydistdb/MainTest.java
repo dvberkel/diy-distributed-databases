0295aef79af372d5179affcd268f4eb0b5c1c645
dvberkel <daan.v.berkel.1980@gmail.com>
2015-07-01 14:07:34 +0100

    Setup a test with mock Nodes
    This test checks to see if everything runs as expected

---
+++ src/test/java/com/github/seeemilyplay/diydistdb/MainTest.java
@@ -0,0 +1,37 @@
+package com.github.seeemilyplay.diydistdb;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class MainTest {
+    public static final int ANY_ID = 1;
+    private ReturnFirst resolver;
+
+    @Before
+    public void createAResolver() {
+        resolver = new ReturnFirst();
+    }
+
+    @Test
+    public void shouldSuccessfullyReadThing() throws Exception {
+        Thing thing = new Thing(1, "foo", 0L);
+        Node node1 = mockedNodeThatReturns(thing);
+        Node node2 = mockedNodeThatReturns(thing);
+        Node node3 = mockedNodeThatReturns(thing);
+
+        Main main = new Main(resolver, new Node[]{ node1, node2, node3 }, 3, 2);
+
+        assertThat(main.read(ANY_ID), is(thing));
+    }
+
+    private Node mockedNodeThatReturns(Thing thing) throws Exception {
+        Node node = mock(Node.class);
+        when(node.getThing(ANY_ID)).thenReturn(thing);
+        return node;
+    }
+}
