fec1b7549094bc2384972f232500ff054d825acd
dvberkel <daan.v.berkel.1980@gmail.com>
2015-07-04 19:28:00 +0200

    Introduce a CommandOption
    CommandOptions help to fascilitate the open for extension closed for
    modification rule

--- src/main/java/com/github/seeemilyplay/diydistdb/ui/CommandLineParser.java
+++ src/main/java/com/github/seeemilyplay/diydistdb/ui/CommandLineParser.java
@@ -6,22 +6,67 @@ import com.github.seeemilyplay.diydistdb.ui.command.GetCommand;
 import com.github.seeemilyplay.diydistdb.ui.command.NOOPCommand;
 import com.github.seeemilyplay.diydistdb.ui.command.PutCommand;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CommandLineParser {
-    private static final Pattern GET_PATTERN = Pattern.compile("get\\s+(\\d+)", Pattern.CASE_INSENSITIVE);
-    private static final Pattern PUT_PATTERN = Pattern.compile("put\\s+(\\d+)\\s+(\\w+)", Pattern.CASE_INSENSITIVE);
 
     public Command parse(String line) {
-        Matcher matcher1 = GET_PATTERN.matcher(line);
-        Matcher matcher2 = PUT_PATTERN.matcher(line);
-        if (matcher1.matches()) {
-            return new GetCommand(Integer.valueOf(matcher1.group(1)));
-        }
-        if (matcher2.matches()) {
-            return new PutCommand(new Thing(Integer.valueOf(matcher2.group(1)), matcher2.group(2)));
+        List<CommandOption> options = Arrays.asList(
+                new Get(line),
+                new Put(line)
+        );
+        for (CommandOption option : options) {
+            if (option.applies()) {
+                return option.command();
+            }
         }
         return new NOOPCommand();
+    }
+}
+
+interface CommandOption {
+    boolean applies();
+    Command command();
+}
+
+abstract class AbstractCommandOption implements CommandOption {
+    protected final Matcher matcher;
+
+    public AbstractCommandOption(Matcher matcher) {
+        this.matcher = matcher;
+    }
+
+    @Override
+    public boolean applies() {
+        return matcher.matches();
+    }
+}
+
+class Get extends AbstractCommandOption {
+    private static final Pattern GET_PATTERN = Pattern.compile("get\\s+(\\d+)", Pattern.CASE_INSENSITIVE);
+
+    public Get(String line) {
+        super(GET_PATTERN.matcher(line));
+    }
+
+    @Override
+    public Command command() {
+        return new GetCommand(Integer.valueOf(matcher.group(1)));
+    }
+}
+
+class Put extends AbstractCommandOption {
+    private static final Pattern PUT_PATTERN = Pattern.compile("put\\s+(\\d+)\\s+(\\w+)", Pattern.CASE_INSENSITIVE);
+
+    public Put(String line) {
+        super(PUT_PATTERN.matcher(line));
+    }
+
+    @Override
+    public Command command() {
+        return new PutCommand(new Thing(Integer.valueOf(matcher.group(1)), matcher.group(2)));
     }
 }
